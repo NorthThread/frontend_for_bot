f17107e06b0016c70ca947ad234017a16b6e8717
NorthThread <Victorm9803@gmail.com>
2021-03-06 19:11:25 +0100

    init for frontend

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app>
+  <v-app v-if="loggedIn != true">
     <v-app-bar app clipped-left clipped-right flat height="50" color="#1E1E1E">
       <img
         src="https://cdn.discordapp.com/avatars/676575206214729748/e2611ef8ccb335797de86727d095d15e.png?size=256"
@@ -23,54 +23,48 @@
         </v-text-field>
       </v-responsive>
     </v-app-bar>
-    <v-navigation-drawer app clipped dark>
+    <v-navigation-drawer app clipped color="#1E1E1E">
       <v-list-item>
         <v-list-item-content>
-          <v-list-item-title class="title">
-            Servers
+          <v-list-item-title class="title" style="color:white;">
+            {{ server }}
           </v-list-item-title>
         </v-list-item-content>
       </v-list-item>
 
-      <v-divider></v-divider>
+      <v-divider light></v-divider>
 
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
+      <v-list v-if="server === 'Not connected'" dense nav>
+        <v-list-item v-for="item in items" :key="item.title" dark>
+          <v-list-item-content color="white">
             <v-list-item-title>{{ item.title }}</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-
-    <v-navigation-drawer clipped app dark right>
-      <v-list-item>
-        <v-list-item-content>
-          <v-list-item-title class="title">
-            Queue
-          </v-list-item-title>
-        </v-list-item-content>
-      </v-list-item>
-
-      <v-divider></v-divider>
-
-      <v-list dense nav>
-        <v-list-item v-for="item in items" :key="item.title" link>
-          <v-list-item-icon>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-icon>
-
-          <v-list-item-content>
-            <v-list-item-title>{{ item.title }}</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
+    
     <v-main> </v-main>
+    <v-footer app inset color="transparent" >
+      <v-app-bar color="#424242" rounded inset clipped right max-width="30%">
+        <v-btn
+          color="grey"
+          width="33%"
+          @click="play(true)"
+          rounded
+        >
+          <v-icon>mdi-play</v-icon>
+        </v-btn>
+        <v-btn color="grey" width="33%" @click="play(false)" rounded>
+          <v-icon>mdi-pause</v-icon>
+        </v-btn>
+        <v-btn color="grey" width=33% rounded>
+          <v-icon>
+            mdi-skip-next
+          </v-icon>
+        </v-btn>
+        
+      </v-app-bar>
+    </v-footer>
   </v-app>
 </template>
 
@@ -87,7 +81,9 @@ export default {
       { title: "Click Me 3" },
       { title: "Click Me 4" },
       { title: "Click Me 2" }
-    ]
+    ],
+    server: "Not connected",
+    loggedIn: false
     //
   }),
   methods: {
---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import Vue from "vue";
+import Vuex from "vuex";
+
+import axios from "axios";
+//import createPersistedState from "vuex-persistedstate";
+
+axios.defaults.withCredentials = true;
+
+Vue.use(Vuex);
